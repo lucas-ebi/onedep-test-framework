d3443559d1e923c0e0b27230ae46208b3b3aaa8c
Lucas Carrijo de Oliveira <lucas@ebi.ac.uk>
2026-04-17 12:24:01 +0100

    Enhance logging functionality: update log file path handling and ensure existing loggers are reconfigured

--- odtf/common.py
+++ odtf/common.py
@@ -1,12 +1,20 @@
 import logging
 
 _log_file = "onedep_test.log"
+_managed_loggers: list = []
 
 
 def set_log_file(path: str):
-    """Set the log file path for all file loggers."""
+    """Set the log file path and update all existing file loggers."""
     global _log_file
     _log_file = path
+    # Re-point all previously created loggers to the new file
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    new_handler = logging.FileHandler(_log_file)
+    new_handler.setFormatter(formatter)
+    for logger in _managed_loggers:
+        logger.handlers.clear()
+        logger.addHandler(new_handler)
 
 
 def get_file_logger(name: str) -> logging.Logger:
@@ -27,5 +35,7 @@ def get_file_logger(name: str) -> logging.Logger:
     
     # Prevent propagation to the root logger
     logger.propagate = False
+
+    _managed_loggers.append(logger)
     
     return logger
